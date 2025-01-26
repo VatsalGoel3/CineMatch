c4910faafcc4459e7a547dd2f0a1eecb18924cc8
Vatsal Goel <144617902+VatsalGoel3@users.noreply.github.com>
2025-01-25 23:35:40 -0700

    feat(recommendations): basic content-based filtering of disliked or watched

---
+++ backend/src/routes/recommendations.js
@@ -0,0 +1,58 @@
+const express = require('express');
+const router = express.Router();
+const axios = require('axios');
+
+const requireAuth = require('../middleware/requireAuth');
+const User = require('../models/User');
+const Movie = require('../models/Movie');
+
+// GET /movies/recommendations
+// Protected route to get recommended movies based on user prefrences
+router.get('/', requireAuth, async (req, res) => {
+    try {
+        const userId = req.user.userId;
+
+        // Fetch user info (including likes/dislikes/watched)
+        const user = await User.findById(userId)
+            .populate('likes')
+            .populate('dislikes')
+            .populate('watched.movieId');
+        if(!user) {
+            return res.status(404).json ({ msg: 'User not found'});
+        }
+
+        // Basic approach: Check user's liked movies -> find similar
+
+        const likedMovieIds = user.likes.map((m) => m._id.toString());
+        const dislikedMovieIds = user.dislikes.map((m) => m._id.toString());
+        const watchedMovieIds = user.watched.map((m) => w.movieId._.id.toString());
+
+        // For test: Only recommend "popular" or "trending" movies and 
+        // exclude watched or disliked
+        const allMovies = await Movie.find().sort({ popularity: -1 }).limit(30).exec();
+
+        const recommend = allMovies.filter((movie) => {
+            const idStr = movie._id.toString();
+            if (dislikedMovieIds.includes(idStr)) return false;
+            if (watchedMovieIds.includes(idStr)) return false;
+            return true;
+        });
+
+        // Return top 10 recommended for test
+        const finalRecs = recommend.slice(0, 10).map((movie) => ({
+            id: movie._id,
+            title: movie.title,
+            poster: movie.posterPath
+                ? `https://image.tmdb.org/t/p/w500${movie.posterPath}`
+                : '',
+            release_date: movie.releaseDate,
+        }));
+
+        return res.status(200).json({ data: finalRecs });
+    } catch (err) {
+        console.error('Recommendation Error:', err);
+        return res.status(500).json({ msg: 'Server error, please try again.' });
+    }
+});
+
+module.exports = router;
